4b260373598252da2da543889fe8c980082bc052
Leopold <leopold.andre@edu.ece.fr>
2023-11-27 22:47:38 +0100

    changement place fichiers, structures operations et stations, sous programme pour compter le nombre de ligne d'un fichier, lecture des operations et stockage dans un tableau de structures

--- lecture_contraintes.c
+++ lecture_contraintes.c
@@ -3,3 +3,47 @@
 //
 #include "lecture_contraintes.h"
 
+t_operation *creer_operation(int num, float temps){
+    t_operation *nouvelle_operation;
+    nouvelle_operation = (t_operation *)malloc(sizeof(t_operation));
+    nouvelle_operation->num = num;
+    nouvelle_operation->temps = temps;
+    nouvelle_operation->rang = 0;
+    //j'initialise le rang de la nouvelle operation à 0 parce que j'ai besoin de lire le graphe de precedences pour pouvoir determiner le rang
+    return nouvelle_operation;
+}
+
+t_operation **lire_operations(char * NomFichier, int *nmb_lignes){
+    FILE * ifs = fopen(NomFichier, "r");
+
+    if(!ifs){
+        printf("erreur ouverture ifs\n");
+        exit(-1);
+    }
+    *nmb_lignes = compter_nombre_operations(ifs);
+    rewind(ifs);
+    int num = 1;
+    float temps = 0;
+    t_operation **operation;
+    operation = (t_operation **)malloc(sizeof(t_operation*) ** nmb_lignes);
+
+    for(int i = 0; i< *nmb_lignes; i++){
+        fscanf(ifs, "%d%f", &num, &temps);
+        operation[i] = creer_operation(num, temps);
+    }
+
+    fclose(ifs);
+    return (t_operation **) operation;
+}
+
+int compter_nombre_operations(FILE * fichier){
+    int c;
+    int nLignes = 0;
+    while((c=fgetc(fichier)) != EOF)
+    {
+        if(c=='\n')
+            nLignes++;
+    }
+    rewind(fichier);
+    return nLignes;
+}
--- main.c
+++ main.c
@@ -3,8 +3,24 @@
 //
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "graphe.h"
+#include "lecture_contraintes.h"
 
 int main(){
+    char nomFichier[50];
+    printf("Entrez le nom du fichier: ");
+    fgets(nomFichier, sizeof(nomFichier), stdin);
+    nomFichier[strcspn(nomFichier, "\n")] = 0;
+    int nmb_lignes;
+    t_operation **operations = lire_operations(nomFichier, &nmb_lignes);
+    for(int i = 0; i<nmb_lignes; i++){
+        printf("%d %f\n", operations[i]->num, operations[i]->temps);
+    }
+    for (int i = 0; i < nmb_lignes; i++) {
+        free(operations[i]);
+    }
+    free(operations);
 
     return 0;
-}  
+}
