4cfa26bb0fefda3ed9b53ead002e044940265675
Leopold <leopold.andre@edu.ece.fr>
2023-12-03 19:34:11 +0100

    ajout des rangs a chaque operations avec le code d'Axel

--- precedence.h
+++ precedence.h
@@ -6,68 +6,8 @@
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_PROJET_TDG_TD7_EQUIPE50_PRECEDENCE_H
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct Noeud {
-    int valeur;
-    struct Noeud* suivant;
-} Noeud;
-
-typedef struct Graphe {
-    int nombreNoeuds;
-    Noeud** listeAdjacence;
-} Graphe;
-Graphe* creerGraphe(int nombreNoeuds);
-
-Graphe* initialiserGraphe(int nombreNoeuds);
-
-void afficherOrdreDesOperations(Graphe* graphe, int* rang);
+#include "file.h"
 
 
-void libererGraphe(Graphe* graphe);
 
-void ajouterArete(Graphe* graphe, int source, int destination);
-int trouverNombreNoeuds(FILE* fichier);
-
-
-void triTopologiqueRecursif(Graphe* graphe, int sommet, int* visite, int* pile, int* indexPile);
-
-void triTopologique(Graphe* graphe, int* rang);
-
-void afficherGraphe(Graphe* graphe,int *rang);
-
-/*
-int main() {
-    FILE *fichier = fopen("precedences.txt", "r");
-
-    if (fichier == NULL) {
-        fprintf(stderr, "Impossible d'ouvrir le fichier.\n");
-        return 1;
-    }
-
-    int nombreTotalNoeuds = trouverNombreNoeuds(fichier);
-
-    // Réinitialiser le curseur du fichier pour lire à partir du début
-    fseek(fichier, 0, SEEK_SET);
-
-    Graphe* graphe = initialiserGraphe(nombreTotalNoeuds);
-
-    int source, destination;
-    while (fscanf(fichier, "%d %d", &source, &destination) == 2) {
-        ajouterArete(graphe, source, destination);
-    }
-
-    fclose(fichier);
-
-    int* rang = (int*)malloc(nombreTotalNoeuds * sizeof(int));
-    triTopologique(graphe, rang);
-
-    // Afficher le rang de chaque opération
-    afficherGraphe(graphe,rang);
-
-    free(rang);
-    libererGraphe(graphe);
-
-    return 0;
-}
-*/
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_PROJET_TDG_TD7_EQUIPE50_PRECEDENCE_H
